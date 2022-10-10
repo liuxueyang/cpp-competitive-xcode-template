488cea3ae4f5afa2f7e3653df021e0763154d07b
liuxueyang <liuxueyang457@gmail.com>
2022-10-10 11:11:31 +0800

    main_input_version

--- Competitive Programming.xctemplate/main_input_version.cpp
+++ Competitive Programming.xctemplate/main_input_version.cpp
@@ -5,54 +5,105 @@
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <set>
-#include <map>
-#include <queue>
-#include <deque>
-#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 #include <cmath>
-#include <iomanip>
-#include <string.h>
+#include <climits>
 
-// Shortcuts for "common" data types in contests
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
 typedef long long ll;
 typedef unsigned long long ull;
-typedef std::vector<int> vi;
-typedef std::vector<ll> vll;
-typedef std::vector<ull> vull;
-typedef std::pair<int, int> pii;
-typedef std::vector<pii> vpii;
-typedef std::set<int> si;
-typedef std::map<std::string, int> msi;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
 
-// To simplify repetitions/loops, Note: define your loop style and stick with it!
-#define REP(i, a, b) \
-for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local!
-#define TRvi(c, it) \
-for (vi::iterator it = (c).begin(); it != (c).end(); it++)
-#define TRvii(c, it) \
-for (vii::iterator it = (c).begin(); it != (c).end(); it++)
-#define TRmsi(c, it) \
-for (msi::iterator it = (c).begin(); it != (c).end(); it++)
-#define INF 2000000000 // 2 billion
+const ull Pr = 131;
 
-// If you need to recall how to use memset:
-#define MEMSET_INF 127 // about 2B
-#define MEMSET_HALF_INF 63 // about 1B
-//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
-//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
-//memset(arr, 0, sizeof arr); // useful to clear array of integers
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
 
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
 
 #pragma mark - MAIN
 
 using namespace std;
 
+<#Write your code here...#>
+
+#ifdef DEBUG
+
 int main(int argc, const char * argv[]) {
-    
+
     #ifdef USE_INPUT_FILE
     freopen("input.txt", "r", stdin);
     #endif
@@ -60,12 +111,12 @@ int main(int argc, const char * argv[]) {
     std::ios::sync_with_stdio(false);
     cout << fixed << setprecision(12);
     cin.tie(NULL);
-    
+
     // MAIN Begin
-    
-    <#Write your code here...#>
-    
+
     // MAIN End
-    
+
     return 0;
 }
+
+#endif
