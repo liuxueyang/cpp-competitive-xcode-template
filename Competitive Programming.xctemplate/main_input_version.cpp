72a5366ac9375d02fdf3a05dab97b8c668b146e6
liuxueyang <liuxueyang457@gmail.com>
2024-01-20 16:47:35 +0800

    Update main input version

--- Competitive Programming.xctemplate/main_input_version.cpp
+++ Competitive Programming.xctemplate/main_input_version.cpp
@@ -1,46 +1,41 @@
 //  ___DATE___
 
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
 #include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
-#include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <map>
-#include <set>
 #include <algorithm>
-#include <utility>
 #include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
 
 using namespace std;
-
-const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
-const double eps = 1e-8;
-const int dir[8][2] = {
-{0, 1}, {0, -1},
-{1, 0}, {-1, 0},
-{1, 1}, {1, -1},
-{-1, 1}, {-1, -1},
-};
 
 typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> VI;
 typedef pair<int, int> PII;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
 
 const ull Pr = 131;
-
-#define LN ListNode
-#define LNP ListNode*
-#define TN TreeNode
-#define TNP TreeNode*
 
 #define For(i, a, b) for (int i = int(a); i < int(b); ++i)
 #define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
@@ -52,52 +47,100 @@ const ull Pr = 131;
 #define f2 second
 #define pb push_back
 #define has(a, x) (a.find(x) != a.end())
-#define nonempty(a) (!a.empty())
-#define all(a) (a).begin(),(a).end()
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
 #define SZ(a) int((a).size())
+#define NL cout << '\n';
 
-#ifdef DEBUG
-#define debug1(x) cout << #x" = " << x << endl;
-#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
-#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V> void __print(const pair<T, V> &x) {
+  cerr << '{';
+  __print(x.first);
+  cerr << ", ";
+  __print(x.second);
+  cerr << '}';
+}
+template <typename T> void __print(const T &x) {
+  int f = 0;
+  cerr << '{';
+  for (auto &i : x)
+    cerr << (f++ ? ", " : ""), __print(i);
+  cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V> void _print(T t, V... v) {
+  __print(t);
+  if (sizeof...(v))
+    cerr << ", ";
+  _print(v...);
+}
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x " = " << x << endl;
+#define debug2(x, y) cout << #x " = " << x << " " #y " = " << y << endl;
+#define debug3(x, y, z)                                                        \
+  cout << #x " = " << x << " " #y " = " << y << " " #z " = " << z << endl;
+#define dbg(x...)                                                              \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = [";    \
+  _print(x);                                                                   \
+  cerr << "\e[39m" << endl;
 #else
 #define debug1
 #define debug2
 #define debug3
+#define dbg(x...)
 #endif
 
-#ifdef DEBUG
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int val) : val(val), next(nullptr) {}
-    ListNode(int val, ListNode *next) : val(val), next(next) {}
-};
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
-#endif
-
-<#Write your code here...#>
-
-int main(int argc, const char * argv[]) {
-    #ifdef USE_INPUT_FILE
-    freopen("input.txt", "r", stdin);
-    #endif
-
-    std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
+void solve() {
     <#Write your code here...#>
-
-    return 0;
 }
 
+int main(void) {
+#ifdef _DEBUG
+  freopen("a.in", "r", stdin);
+#endif
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int T = 1;
+  cin >> T;
+
+  while (T--) {
+    solve();
+  }
+
+  return 0;
+}
